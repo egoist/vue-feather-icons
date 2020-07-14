2f879e02128db56ae2fa7ececde20ed73480cb35
zhuowenli <zhuowenligg@gmail.com>
2020-07-14 10:43:37 +0800

    :sparkles: 兼容 Vue3

--- build.js
+++ build.js
@@ -5,49 +5,48 @@ const fs = require('fs-extra')
 
 const componentTemplate = (name, svg) => `
 export default {
-  name: '${name}',
-  
-  props: {
-    size: {
-      type: String,
-      default: '24',
-      validator: (s) => (!isNaN(s) || s.length >= 2 && !isNaN(s.slice(0, s.length -1)) && s.slice(-1) === 'x' )
+    name: '${name}',
+
+    props: {
+        size: {
+            type: String,
+            default: '24',
+            validator: (s) => (!isNaN(s) || s.length >= 2 && !isNaN(s.slice(0, s.length -1)) && s.slice(-1) === 'x' )
+        }
+    },
+
+    setup(props, ctx) {
+        const size = props.size.slice(-1) === 'x'
+            ? props.size.slice(0, props.size.length -1) + 'em'
+            : parseInt(props.size) + 'px';
+
+        const attrs = ctx.attrs || {}
+        attrs.width = attrs.width || size
+        attrs.height = attrs.height || size
+        ctx.attrs = attrs
+
+        return ${svg.replace(/<svg([^>]+)>/, '<svg$1 {...ctx.attrs}>')}
     }
-  },
-
-  functional: true,
-
-  render(h, ctx) {
-    const size = ctx.props.size.slice(-1) === 'x' 
-      ? ctx.props.size.slice(0, ctx.props.size.length -1) + 'em'
-      : parseInt(ctx.props.size) + 'px';
-
-    const attrs = ctx.data.attrs || {}
-    attrs.width = attrs.width || size
-    attrs.height = attrs.height || size
-    ctx.data.attrs = attrs
-  
-    return ${svg.replace(/<svg([^>]+)>/, '<svg$1 {...ctx.data}>')}
-  }
 }
 `.trim()
 
 const handleComponentName = name => name.replace(/\-(\d+)/, '$1')
 
 const icons = Object.keys(feather.icons).map(name => ({
-  name,
-  pascalCasedComponentName: pascalCase(`${handleComponentName(name)}-icon`)
+    name,
+    pascalCasedComponentName: pascalCase(`${handleComponentName(name)}-icon`)
 }))
 
 Promise.all(icons.map(icon => {
-  const svg = feather.icons[icon.name].toSvg()
-  const component = componentTemplate(icon.pascalCasedComponentName, svg)
-  const filepath = `./src/components/${icon.pascalCasedComponentName}.js`
-  return fs.ensureDir(path.dirname(filepath))
-    .then(() => fs.writeFile(filepath, component, 'utf8'))
+    const svg = feather.icons[icon.name].toSvg()
+    const component = componentTemplate(icon.pascalCasedComponentName, svg)
+    const filepath = `./src/components/${icon.pascalCasedComponentName}.js`
+
+    return fs.ensureDir(path.dirname(filepath)).then(() => fs.writeFile(filepath, component, 'utf8'))
 })).then(() => {
-  const main = icons
-    .map(icon => `export { default as ${icon.pascalCasedComponentName} } from '../icons/${icon.pascalCasedComponentName}'`)
-    .join('\n\n')
-  return fs.outputFile('./src/index.js', main, 'utf8')
+    const main = icons
+        .map(icon => `export { default as ${icon.pascalCasedComponentName} } from '../icons/${icon.pascalCasedComponentName}'`)
+        .join('\n\n');
+
+    return fs.outputFile('./src/index.js', main, 'utf8')
 })
